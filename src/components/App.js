d5995765aa4138854f79b60f8e5f45ee51da1b1c
Blessymol Simon <blessy4mooc@gmail.com>
2020-11-12 22:33:38 +0530

    1st edits

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,47 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 class App extends Component {
     render() {
+        constructor(props)
+        {
+            Super(props);
+            this.state = {
+                time: new Date()
+            }
+            this.intervalid = null;
+        }
+        return (
+            <div className="Clock">
+                <h3 id="time">{this.getTimeString()}</h3>
+            </div>
+        );
+    }
+    componentDidMount() {
+        this.intervalid= setInterval(
+            () => this.setState({time: new Date()}),
+            1*1000
+        );
+    }
 
-        return(
-            <>
-               
-            </>
-        )
+    componentWillUnmount() {
+        clearInterval(this.intervalid);
+    }
+
+    getTimeString() {
+        const currTime = this.state.time;
+        const [hrs, mins, secs] = [currTime.getHours(), currTime.getMinutes(), currTime.getSeconds()];
+        const amOrpm = hrs > 12 ? "AM" : "PM";
+        const hrstwelveHrForm = hrs>12 ? hrs - 12 : hrs;
+        const hrString=this.toTwoDigits(hrstwelveHrForm);
+        const minString=this.toTwoDigits(mins);
+        const secString=this.toTwoDigits(secs);
+        const timeString = `${hrString}:${minString}:${secString} ${amOrpm}`;
+        return timeString;
+    }
+
+    toTwoDigits(num) {
+        return ((num<10 ? "0":"")+num);
     }
 }
 
